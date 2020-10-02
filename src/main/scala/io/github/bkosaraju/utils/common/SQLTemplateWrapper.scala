720ef34a20a89ea9527763f829f9ccd26eb18742
Bhargav Kosaraju <bhargav_kosaraju@worksafe.vic.gov.au>
2020-10-03 09:29:30 +1000

    Templating engine changed from scalate to mustache due to runtime exeuction issues

--- src/main/scala/io/github/bkosaraju/utils/common/SQLTemplateWrapper.scala
+++ src/main/scala/io/github/bkosaraju/utils/common/SQLTemplateWrapper.scala
@@ -22,25 +22,27 @@
 
 package io.github.bkosaraju.utils.common
 
-import java.io.File
+import java.io.{File, StringReader, StringWriter}
 
-import org.fusesource.scalate._
+//import org.fusesource.scalate._
 import java.nio.file.{Files, Path}
 
 import io.github.bkosaraju.utils.aws.AwsUtils
+import com.github.mustachejava.DefaultMustacheFactory
 import org.apache.commons.io.FileUtils.copyFile
-//import io.github.bkosaraju.utils.spark.Context
+
+import scala.collection.JavaConverters.mapAsJavaMapConverter
+//import au.gov.wsv.imf.utils.spark.Context
 import com.amazonaws.services.s3.AmazonS3URI
 import org.apache.commons.io.FileUtils
 
 trait SQLTemplateWrapper
   extends Session {
-    //with Context {
+  //with Context {
   /**
    * Templating Engine to Convert the SQL template into Spark Consumable SQL
    *
    * @param config   - Properties which shall be replaced into the template
-   * @param templateType - template type as such mustache,ssp, scaml etc ..
    * @return SQL String with template substituted version.
    *         {{{
    *                val templateFileLocation = "/tmp/template/" + UUID.randomUUID().toString
@@ -56,10 +58,11 @@ trait SQLTemplateWrapper
    *                return sqlText
    *         }}}
    */
-  def getSQLFromTemplate(config : Map[String,String], templateType : String = "mustache"): String = {
+  def getSQLFromTemplate( config : Map[String,String] ): String = {
     val templateFileLocation =  Files.createTempDirectory("")
     try {
-      val templateFile = templateFileLocation + "/sqlFile.template"
+
+      val templateFile = templateFileLocation + "/sqlFile.mustache"
       val inputSQLFile = config("sqlFile")
       if (inputSQLFile.toLowerCase.toLowerCase.matches("s3[a]?:.*")) {
         val S3URI = new AmazonS3URI(
@@ -72,23 +75,32 @@ trait SQLTemplateWrapper
         copyFile(new File(config("sqlFile")),new File(templateFile))
       }
       //Disabled to avoid hadoop dependency
-//      else {
-//        hadoopfs.copyToLocalFile(new Path(config("sqlFile")), new Path(templateFile))
-//      }
-      val engine = new TemplateEngine
-      engine.workingDirectory = templateFileLocation.toFile
-      val src = engine.source(templateFile, templateType)
-      val sqlText = engine.layout(src, config)
-      engine.shutdown()
+      //      else {
+      //        hadoopfs.copyToLocalFile(new Path(config("sqlFile")), new Path(templateFile))
+      //      }
+
+
+      //      val engine = new TemplateEngine
+      //      engine.workingDirectory = templateFileLocation.toFile
+      //      val src = engine.source(templateFile, templateType)
+      //      val sqlText = engine.layout(src, config)
+      //      engine.shutdown()
+
+      val mustacheFactory = new DefaultMustacheFactory()
+      val mustache = mustacheFactory.compile(new StringReader(FileUtils.readFileToString(new File(templateFile))),"template.file")
+      val stringWriter = new StringWriter()
+      mustache.execute(stringWriter,config.asJava).flush()
       Thread.sleep(1000)
       FileUtils.deleteDirectory(templateFileLocation.toFile)
-      sqlText
+      stringWriter.toString
     } catch {
       case e: Throwable => {
         FileUtils.deleteDirectory(templateFileLocation.toFile)
         logger.error("Unable to Generate the Template From Given Input File : "+config.getOrElse("sqlFile","Not Provided"))
         throw e
       }
+    } finally {
+      FileUtils.deleteDirectory(templateFileLocation.toFile)
     }
   }
-  }
+}
